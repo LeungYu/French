56e4e9426209be8f4d4c5b6e76a0cb66867217a1
LeungYu <20152005050@m.scnu.edu.cn>
2018-05-04 21:11:40 +0800

    更正

---
+++ app/src/main/java/com/glf/roideladictee/Json/Get_User_Contest_Json.java
@@ -0,0 +1,11 @@
+package com.glf.roideladictee.Json;
+
+/**
+ * Created by lenovo on 2017/12/6.
+ */
+
+public class Get_User_Contest_Json {
+    public String contest_id;
+    public String phone_number;
+    public String movie_name;
+}
